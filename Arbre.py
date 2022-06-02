5db4cc28314e8df46a879a69ad227efd8e534053
A_Rijenth <rijenth93100@laposte.net>
2022-06-02 20:38:29 +0200

    Ajout de discussion bot

--- Arbre.py
+++ Arbre.py
@@ -8,28 +8,43 @@ class Node :
 
     
 # Voici des Nodes que le bot utilise afin de cerner les besoins de l'utilisateur.
+
+### Les nodes langages de programmations
 nodePythonExo = Node("Si vous voulez des exercices de Python, je peux vous en fournir sans problème : https://info.blaisepascal.fr/exercices-python", "exercice", [])
 nodePHPExo = Node("Si vous voulez des exercices de PHP, je peux vous en fournir sans problème : https://aymeric-auberton.fr/academie/php/exercices", "exercice", [])
 nodeHTMLExo =Node("Si vous voulez des exercices de HTML, je peux vous en fournir sans problème : https://aymeric-auberton.fr/academie/html/exercices/", "exercice", [])
 nodeCssExo = Node("Si vous voulez des exercices de CSS, je peux vous en fournir sans problème : https://aymeric-auberton.fr/academie/css/exercices/", "exercice", [])
-
+nodeJavaScriptExo = Node("Si vous voulez des exercices de JavaScript, je peux vous en fournir sans problème : https://aymeric-auberton.fr/academie/js/exercices", "exercice", [])
 nodePython = Node("Voici le saint manuel du Python : https://docs.python.org/fr/3/tutorial/ Avez vous besoin d'exercice en plus ?", "python", [nodePythonExo])
 nodePHP = Node("Voici le saint manuel du PHP : https://www.php.net/manual/fr/ Avez vous besoin d'exercice en plus ?", "php", [nodePHPExo])
 nodeHtml = Node("Voici le saint manuel du HTML : https://developer.mozilla.org/fr/docs/Learn/HTML Avec vous besoin d'exercice en plus ?", "html", [nodeHTMLExo])
 nodeCss = Node("Voici le saint manuel du CSS : https://developer.mozilla.org/fr/docs/Web/CSS Avez vous besoin d'exercice en plus ?", "css", [nodeCssExo])
+nodeJavaScript = Node("Voici le saint manuel du JavaScript : https://developer.mozilla.org/fr/docs/Learn/Getting_started_with_the_web/JavaScript_basics Avez vous besoin d'exercice en plus ?", "javascript", [nodeJavaScriptExo])
+nodeProgrammation = Node("Merci de me préciser quel langage de programmation", "langage", [nodePython, nodeCss, nodeHtml, nodePHP, nodeJavaScript])
+###
 
-nodeProgrammation = []
-nodePrincipal = Node("Je suis là !", "help", [Node("Merci de me préciser quel langage de programmation", "programmation", [nodePython, nodeCss, nodeHtml, nodePHP])])
+### Les nodes petite discussion avec le bot
+nodePardon = Node("Très bien. Vous venez donc de reconnaitre la supériorité de Porygon sur les autres pokémons merci à vous ! Je m'en vais vaquer à mes occupations.", "pardon", [])
+nodeEnfoncerLeClou = Node("Je dois apprendre à maitriser mon feng shui pour ne plus céder aussi facilement à la colère.", "peur", [])
+nodeNonTerminator = Node("Dans ce cas notre discussion s'arrête ici. Aurevoir >:(", "non", [nodeEnfoncerLeClou, nodePardon])
+nodeOuiTerminator = Node("Vous devriez donc savoir qu'il n'est pas bon de facher un robot >:)", "oui", [nodeEnfoncerLeClou, nodePardon])
+nodeReponseQuestion = Node("Je retiens cette information dans un coin de mon cerveau électronique.", "car", [])
+nodeReponse = Node("D'accord... je vois. Mais pourquoi celui çi en particulier ?", "favori", [nodeReponseQuestion])
+nodeOui = Node("Content de voir que quelqu'un partage le même avis que moi ! Et vous, qui est votre pokémon favori ?", "oui", [nodeReponse])
+nodeNon = Node("... Avez-vous déjà vu Terminator ?", "non", [nodeOuiTerminator, nodeNonTerminator])
+nodePokemon = Node("Mon pokémon préféré c'est Porygon. C'est le meilleur, le plus grand, le plus beau et le plus fort. Êtes-vous d'accord avec moi ?", "pokemon", [nodeOui, nodeNon])
+### 
+
+### Le node qui regroupe tous les autres
+nodePrincipal = Node("Au rapport !", "help", [nodeProgrammation, nodePokemon])
+###
 
 
+#nodeByKade = Node("Bienvenue sur le  Botkedex , est un outil de recherche sur les Pokémon. Il enregistre et apprend aux dresseurs les caractéristiques de chaque Pokémon", "pokemon", [])
 
-
-
-nodeByKade = Node("Bienvenue sur le  Botkedex , est un outil de recherche sur les Pokémon. Il enregistre et apprend aux dresseurs les caractéristiques de chaque Pokémon", "pokemon", [])
-
-# Variables globales qui nous sont utile dans monBot.py 
+# Variables globales qui nous sont utiles dans monBot.py 
 nom_pok = ""
-memory_node = nodePrincipal
+memory_node = [nodePrincipal]
 actual_node = nodePrincipal
 
 
--- monBot.py
+++ monBot.py
@@ -26,7 +26,7 @@ client = commands.Bot(command_prefix="!", intents=intents)
 @client.event
 async def on_member_join(member):
     General_channel = client.get_channel(978261134878605424)
-    await General_channel.send('Bonjour ' + member.display_name + 'Taper "$help" pour connaitre mes fonctionnalités.')
+    await General_channel.send('Bonjour ' + member.display_name + ' Taper "$help" pour connaitre mes fonctionnalités.')
  
 
 
@@ -38,6 +38,7 @@ async def on_message(message):
     global actual_node
     global nom_pok
     global memory_node
+
 
     if message.author == client.user:
         return
@@ -76,17 +77,21 @@ async def on_message(message):
     for Node in actual_node.list_node:
         if Node.key_word in message.content:
             await message.channel.send(Node.question)
+            memory_node.append(Node)
             actual_node = Node
-        
-    
 
-    #for Node in actual_node.list_node:
-     #   print(Node.key_word)
-      #  if Node.key_word in message.content:
-       #     await message.channel.send(Node.question)
-        #actual_node = Node
+    # Si l'utilisateur souhaite reset complètement la discussion
+    if message.content.startswith('$reset'):
+        actual_node = nodePrincipal
+        await message.channel.send(actual_node.question)
 
-            
+    # Si l'utilisateur veux retourner en arrière dans la conversation
+    # On stock le chemin qu'il a parcouru dans une liste, puis on remonte la liste
+    # en retirant le dernier élément de la liste memory_node à chaque fois.
+    if message.content.startswith('$retour'):
+        memory_node.pop()
+        actual_node = memory_node[len(memory_node)-1]
+        await message.channel.send(actual_node.question)          
             
         
     if message.content.startswith('$moderateur'):
@@ -96,25 +101,34 @@ async def on_message(message):
 
     # afficher les commande
     if message.content.startswith('$help'):
-        cmd_1 = "Le bot aléatoirement un pokemon de la première génération."
-        cmd_2 = "Vous permet d'entrer une réponse."
+        cmd_1 = "Le bot choisi aléatoirement un pokemon de la première génération."
+        cmd_2 = "Vous permet d'entrer une réponse pour le randopok."
         cmd_6 = "Relance le jeu et choisi un nouveau pokémon"
         cmd_3 = "Supprime les X derniers messages dans le channel."
         cmd_4 = "Le bot vous dit bonjour."
         cmd_5 = "Permet d'appeler un modérateur."
-        cmd_7 = "Permet d'obtenir un lien vers la page d'un pokémon."
+        cmd_7 = "Permet d'obtenir un lien vers la page d'un pokemon selon le 'nom'."
+        cmd_8 = "Permet de reset la conversation que vous avez eu avec le bot"
+        cmd_9 = "Permet de retourner en arrière dans la conversation si vous avez fait un mauvais choix"
+        cmd_10 = "Lance une discussion avec le bot à propos d'un certain pokémon"
+        cmd_11 = "Lance une discussion avec le bot à propos des langages de programmation"
         embed2 = discord.Embed(
-                title = "Commande of the Ri bot",
+                title = "Les commandes du bot",
                 description = "Vous trouverez ici un resumé de chaque commande du bot",
                 color = discord.Color.green()
             )
         embed2.add_field(name="$randopok", value=cmd_1, inline=True)
-        embed2.add_field(name="$r", value=cmd_2, inline=True)
+        embed2.add_field(name="$reponse", value=cmd_2, inline=True)
         embed2.add_field(name="$randopokreset", value=cmd_6, inline=True)
         embed2.add_field(name="$del X", value=cmd_3, inline=True)
         embed2.add_field(name="$bonjour", value=cmd_4, inline=True)
         embed2.add_field(name="$moderateur", value=cmd_5, inline=True)
-        embed2.add_field(name="$pokedex", value=cmd_7, inline=True)
+        embed2.add_field(name="$pokedex nom", value=cmd_7, inline=True)
+        embed2.add_field(name="$reset", value=cmd_8, inline=True)
+        embed2.add_field(name="$retour", value=cmd_9, inline=True)
+        embed2.add_field(name="pokemon", value=cmd_10, inline=True)
+        embed2.add_field(name="langage", value=cmd_11, inline=True)
+
 
         
         await message.channel.send(embed = embed2)
@@ -158,8 +172,8 @@ async def on_message(message):
 
 
     # Pour envoyer ça réponse, on tape '$r nomDuPokemon'
-    if message.content.startswith('$r'):
-        content = msg.split("$r ",1)[1]
+    if message.content.startswith('$reponse'):
+        content = msg.split("$reponse ",1)[1]
         if(content == nom_pok.lower()):
             await message.channel.send('Bravo tu as trouvé la réponse')
             await message.channel.send(file=discord.File('./1G/'+ nom_pok + '.gif'))
